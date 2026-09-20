5e10b9c537ee561263b9bfb35b7ed4871b0c87bc
akshayDatacode <akshay.webllisto@gmail.com>
2026-09-20 18:00:47 +0530

    stock reset issue fix try

--- src/database/database.module.ts
+++ src/database/database.module.ts
@@ -10,6 +10,10 @@ import { ConfigService } from '@nestjs/config';
         uri: configService.get<string>('MONGO_URI'), // Fetch the URI from environment variables
         retryWrites: true, // Ensure retryWrites is enabled for transactions
         w: 'majority', // Write concern for transactions
+        serverSelectionTimeoutMS: 5000, // Timeout after 5 seconds if MongoDB is unreachable
+        socketTimeoutMS: 45000, // Close sockets after 45 seconds of inactivity
+        connectTimeoutMS: 10000, // Timeout after 10 seconds for initial connection
+        maxPoolSize: 10, // Limit the number of connections in the pool
       }),
       inject: [ConfigService],
     }),
